2ab7200c734209ebe4cc433202c84b2de845751a
MALB008 <110874128+MALB008@users.noreply.github.com>
2024-10-10 17:34:36 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,29 +1,29 @@
-var vg_1 = "./vegalite/choropleth_global_crop_yield.vl.json";
+var vg_1 = "choropleth_global_crop_yield.vl.json";
 vegaEmbed("#choropleth", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
 
-var vg_1 = "./vegalite/Isotype.vl.json";
+var vg_1 = "Isotype.vl.json";
 vegaEmbed("#isotype", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
 
-var vg_1 = "./vegalite/linechart.json";
+var vg_1 = "linechart.json";
 vegaEmbed("#linechart", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
 
-var vg_1 = "./vegalite/stackedbarchart_summer.vl.json";
+var vg_1 = "stackedbarchart_summer.vl.json";
 vegaEmbed("#stackedbarsummer", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
 
-var vg_1 = "./vegalite/stackedbarchart_winter.vl.json";
+var vg_1 = "stackedbarchart_winter.vl.json";
 vegaEmbed("#stackedbarwinter", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
 
-var vg_1 = "./vegalite/dashboxes.json";
+var vg_1 = "dashboxes.json";
 vegaEmbed("#boxes", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
