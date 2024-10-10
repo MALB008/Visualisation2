33cd488c92e0fff3815de20fab42cae3a1bb61da
MALB008 <110874128+MALB008@users.noreply.github.com>
2024-10-10 17:33:30 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,29 @@
+var vg_1 = "./vegalite/choropleth_global_crop_yield.vl.json";
+vegaEmbed("#choropleth", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+var vg_1 = "./vegalite/Isotype.vl.json";
+vegaEmbed("#isotype", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+var vg_1 = "./vegalite/linechart.json";
+vegaEmbed("#linechart", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+var vg_1 = "./vegalite/stackedbarchart_summer.vl.json";
+vegaEmbed("#stackedbarsummer", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+var vg_1 = "./vegalite/stackedbarchart_winter.vl.json";
+vegaEmbed("#stackedbarwinter", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+var vg_1 = "./vegalite/dashboxes.json";
+vegaEmbed("#boxes", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
